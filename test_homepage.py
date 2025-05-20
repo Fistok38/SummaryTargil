1f7bf7702de141c747c7edee96fad37317d75a6c
Fistok38 <ahozavsky@gmail.com>
2025-05-20 20:51:23 +0300

    Added home page test

---
+++ test_homepage.py
@@ -0,0 +1,3 @@
+def test_homepage_loads(driver):
+    driver.get("https://www.thebear.co.il/")
+    assert "מוצרי הרבלייף במחירים מעולים| ®THE BEAR | משלוח עד הבית בחינם – The Bear" in driver.title or driver.find_element("css selector", "header")
